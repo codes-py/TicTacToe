1a65b41580b87cd222723017f8d98e26da24c226
codes-py <63456076+codes-py@users.noreply.github.com>
2021-05-07 14:28:10 +0530

    Included round counter

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-let ttt = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-let clickCount = 0, streak = 0;
+let ttt;
+let clickCount;
 let winner, preWinner;
+let matchCount = -1, streak = 0;
 
-const PLAYERS = ['X', 'O'], COLOR = ['danger', 'primary'];
+const PLAYERS = ['X', 'O'], CSS = ['danger', 'primary'], PLCOLOR = ["Red", "Blue"];
 
 let btns = document.getElementsByTagName('button');
-let p1 = document.getElementById('Player 1');
-let p2 = document.getElementById('Player 2');
+let p1 = document.getElementById('Red');
+let p2 = document.getElementById('Blue');
 let res = document.getElementById('res');
 let turn = document.getElementById('turn');
 let again = document.getElementById('again')
+let match = document.getElementById('matchCount');
 
-function starter() {
+function play() {
     playAgain();
+    document.getElementById('roundIndicator').style.display = '';
     document.getElementById('start').style.display = 'none';
 }
 
@@ -21,11 +24,11 @@ function buttonClick(btn) {
                 btn.id[btn.id.length - 1] - 1
             ];
     ttt[pos[0]][pos[1]] = PLAYERS[clickCount % 2];
-    btn.className = `btn-lg btn-${COLOR[clickCount % 2]}`;
+    btn.className = `btn-lg btn-${CSS[clickCount % 2]}`;
     btn.append(ttt[pos[0]][pos[1]]);
     btn.disabled = true;
     clickCount++;
-    turn.textContent = PLAYERS[clickCount  % 2] + ' is playing...';
+    turn.textContent = PLCOLOR[clickCount  % 2] + ' is playing...';
     winner = decideWinner();
     if (winner) {
         updateScores();
@@ -38,32 +41,32 @@ function decideWinner() {
         for (let i = 0; i < 3; i++) {
             if (ttt[i][0] === ttt[i][1] && 
                 ttt[i][0] === ttt[i][2])
-                if (ttt[i][0] === 'X')
-                    return 'Player 1';
-                else if (ttt[i][0] === 'O')
-                    return 'Player 2';
+                if (ttt[i][0] === PLAYERS[0])
+                    return 'Red';
+                else if (ttt[i][0] === PLAYERS[1])
+                    return 'Blue';
         }
 
         for (let j = 0; j < 3; j++) {
             if (ttt[0][j] === ttt[1][j] && 
                 ttt[0][j] === ttt[2][j])
-                if (ttt[0][j] === 'X')
-                    return 'Player 1';
-                else if (ttt[0][j] === 'O')
-                    return 'Player 2';
+                if (ttt[0][j] === PLAYERS[0])
+                    return 'Red';
+                else if (ttt[0][j] === PLAYERS[1])
+                    return 'Blue';
         }
 
         if (ttt[0][0] === ttt[1][1] &&
             ttt[2][2] === ttt[1][1] ||
             ttt[0][2] === ttt[1][1] &&
             ttt[2][0] === ttt[1][1])
-            if (ttt[1][1] === 'X')
-                return 'Player 1';
-            else if (ttt[1][1] === 'O')
-                return 'Player 2';        
+            if (ttt[1][1] === PLAYERS[0])
+                return 'Red';
+            else if (ttt[1][1] === PLAYERS[1])
+                return 'Blue';        
     }
 
-    if (clickCount === 9) {
+    if (clickCount === 9 + matchCount % 2) {
         winner = undefined;
         updateScores();
         finishGame('Game Draw');
@@ -77,11 +80,11 @@ function updateScores() {
     else
         streak = 1
 
-    if (winner === 'Player 1') {
+    if (winner === 'Red') {
         p1.children.wins.textContent = Number(p1.children.wins.textContent) + 1;
         p2.children.losses.textContent = Number(p2.children.losses.textContent) + 1;
     }
-    else if (winner === 'Player 2') {
+    else if (winner === 'Blue') {
         p2.children.wins.textContent = Number(p2.children.wins.textContent) + 1;
         p1.children.losses.textContent = Number(p1.children.losses.textContent) + 1;
     }
@@ -100,15 +103,17 @@ function finishGame(winMessage) {
 }
 
 function playAgain() {
+    matchCount += 1;
     for (let i = 0; i < btns.length - 1; i ++) {
         btns[i].className = 'btn btn-secondary'
         btns[i].textContent = ''
         btns[i].disabled = false;
     }
+    match.textContent = matchCount + 1;
     res.textContent = '';
     ttt = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-    clickCount = 0;
-    turn.textContent = PLAYERS[clickCount  % 2] + ' is playing...';
+    clickCount = 0 + matchCount % 2;
+    turn.textContent = PLCOLOR[clickCount  % 2] + ' is playing...';
     preWinner = winner;
     winner = undefined;
     again.style.display = 'none';
